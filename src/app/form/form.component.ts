d96bde918451491506e5812958cc641c9658a318
vitalsiigns2112 <vitalsigns2112@outlook.com>
2020-11-19 11:08:53 -0300

    Ajustes accordion

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -19,27 +19,50 @@ export class FormComponent implements OnInit {
   }
 
   openBox(value) {
-    this.accordionOne = false;
-    this.accordionTwo = false;
-    this.accordionThree = false;
-    this.accordionFour = false;
 
 
-    switch(value) {
-      case 'accordionOne':
+    if(value == 'accordionOne') {
+      if(this.accordionOne) {
+        this.accordionOne = false;
+      } else {
         this.accordionOne = true;
-        break;
-      case 'accordionTwo':
-        this.accordionTwo = true;
-        break;
-      case 'accordionThree':
-        this.accordionThree = true;
-        break;
-      case 'accordionFour':
-        this.accordionFour = true;
-        break;
+      }
+      this.accordionTwo = false;
+      this.accordionThree = false;
+      this.accordionFour = false;
 
+    } else if(value == 'accordionTwo') {
+      if(this.accordionTwo) {
+        this.accordionTwo = false;
+      } else {
+        this.accordionTwo = true;
+      }
+      this.accordionOne = false;
+      this.accordionThree = false;
+      this.accordionFour = false;
+
+    } else if(value == 'accordionThree') {
+      if(this.accordionThree) {
+        this.accordionThree = false;
+      } else {
+        this.accordionThree = true;
+      }
+      this.accordionOne = false;
+      this.accordionTwo = false;
+      this.accordionFour = false;
+
+    } else {
+      if(this.accordionFour) {
+        this.accordionFour = false;
+      } else {
+        this.accordionFour = true;
+      }
+
+      this.accordionOne = false;
+      this.accordionTwo = false;
+      this.accordionThree = false;
     }
+
 
   }
 

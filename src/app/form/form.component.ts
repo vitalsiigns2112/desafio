b1a2cfd0602ed8f348b595486b4b0195ab4c1d3b
vitalsiigns2112 <vitalsigns2112@outlook.com>
2020-11-19 10:42:22 -0300

    Ajustes map e accordion

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  accordionOne: boolean = false;
+  accordionTwo: boolean = false;
+  accordionThree: boolean = false;
+  accordionFour: boolean = false;
+  isOpen: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openBox(value) {
+    this.accordionOne = false;
+    this.accordionTwo = false;
+    this.accordionThree = false;
+    this.accordionFour = false;
+
+
+    switch(value) {
+      case 'accordionOne':
+        this.accordionOne = true;
+        break;
+      case 'accordionTwo':
+        this.accordionTwo = true;
+        break;
+      case 'accordionThree':
+        this.accordionThree = true;
+        break;
+      case 'accordionFour':
+        this.accordionFour = true;
+        break;
+
+    }
+
   }
 
 }

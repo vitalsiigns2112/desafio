e69ba2e5c88e600cf50825bd443d0de430c0d6f6
vitalsiigns2112 <vitalsigns2112@outlook.com>
2020-11-20 12:29:17 -0300

    validação de formulário

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  // variáveis formulário
+  inputName;
+  inputEmail;
+  inputPhone;
+  inputTextarea;
+
+  // variáveis accordion
   accordionOne: boolean = false;
   accordionTwo: boolean = false;
   accordionThree: boolean = false;
@@ -16,6 +23,71 @@ export class FormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    //Inicia as variáveis do formulário quando o component é iniciado
+    this.inputName = document.querySelector("#input-name");
+    this.inputEmail = document.querySelector("#input-email");
+    this.inputPhone = document.querySelector("#input-phone");
+    this.inputTextarea = document.querySelector("#input-textarea");
+  }
+
+  setCustom(element) {
+
+    switch(element) {
+      case 'name':
+        this.inputName.setCustomValidity('Por favor, preencha o campo nome');
+        break;
+      case 'email':
+        this.inputEmail.setCustomValidity('Por favor, preencha o campo email, no padrão correto');
+        break;
+      case 'phone':
+        this.inputPhone.setCustomValidity('por favor, digite o número no padrão XXXX-XXXX');
+        break;
+      case 'textarea':
+        this.inputTextarea.setCustomValidity('Por favor, preencha o campo mensagem');
+        break;
+    }
+
+  }
+
+  clearCustom(element) {
+    switch(element) {
+      case 'name':
+        this.inputName.setCustomValidity('');
+        break;
+      case 'email':
+        this.inputEmail.setCustomValidity('');
+        break;
+      case 'phone':
+        this.inputPhone.setCustomValidity('');
+        break;
+      case 'textarea':
+        this.inputTextarea.setCustomValidity('');
+        break;
+    }
+  }
+
+  onSubmit() {
+
+    // Esse código não vai ser chamado porque a validação tá sendo feita pelo método setCustomValidity()
+    // Caso queira verificar a validação básica só com javascript, é só tirar o atributo required dos campo input e textarea
+    if(this.inputName.value == "") {
+      alert("Por favor, preencha o campo nome");
+      return false;
+    }
+    if(this.inputEmail.value == "") {
+      alert("Por favor, preencha o campo email");
+      return false;
+    }
+    if(this.inputPhone.value == "") {
+      alert("Por favor, preencha o campo telefone");
+      return false;
+    }
+    if(this.inputTextarea.value == "") {
+      alert("Por favor, preencha o campo mensagem");
+      return false;
+    }
+
   }
 
   openBox(value) {
@@ -65,5 +137,6 @@ export class FormComponent implements OnInit {
 
 
   }
+
 
 }
